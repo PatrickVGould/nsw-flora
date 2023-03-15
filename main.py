c67594a13b57fb8e94fd3a89a56267bce8fa9653
PatrickVGould <125094430+PatrickVGould@users.noreply.github.com>
2023-03-15 11:53:52 +1100

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if user_input:
     
     #Clear the field after submission
     input_container.empty()
-    user_input = get_text(input_container)
+    user_input = get_text()
 
 if st.session_state["generated"]:
 
