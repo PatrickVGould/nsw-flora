3ae0eb446ad9318eedbe85159dc57313dd85f1cf
PatrickVGould <125094430+PatrickVGould@users.noreply.github.com>
2023-03-15 11:12:05 +1100

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,6 @@ if st.session_state["generated"]:
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i),
                 avatar_style = "bottts-neutral",
-                seed=1234345
+                seed=765
                 )
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
